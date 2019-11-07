4ee00af0b33a10537b4c20b82d143fd8cd4f8ef9
Dean Wills <dean.wills@appdynamics.com>
2019-11-07 08:56:20 -0800

    initial clean push

--- heap_test.go
+++ heap_test.go
@@ -2,43 +2,64 @@
 // All Rights Reserved, except as explicitly granted via the project license.
 //
 package math
-import "testing"
 
-func TestHeap(t *testing.T) {
-    type args struct {
-        t    interface{}
-        run  HeapCallback
-        a    []interface{}
-        size int
-    }
-    tests := []struct {
-        name string
-        args args
-    }{
-        // TODO: Add test cases.
-    }
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-        })
-    }
-}
+import (
+	"testing"
+)
 
-func Test_rankParksWills0(t *testing.T) {
-    type args struct {
-        arr []int
-    }
-    tests := []struct {
-        name string
-        args args
-        want int
-    }{
-        // TODO: Add test cases.
-    }
-    for _, tt := range tests {
-        t.Run(tt.name, func(t *testing.T) {
-            if got := rankParksWills0(tt.args.arr); got != tt.want {
-                t.Errorf("rankParksWills0() = %v, want %v", got, tt.want)
-            }
-        })
-    }
+// verifies that heapPermutation tries every possible permutation exactly once.
+func Test_HeapPermutation(t *testing.T) {
+	type TestData struct {
+		t *testing.T
+		set []bool
+	}
+	run := func(td interface{}, a []interface{}) {
+		testData := td.(*TestData)
+		arr := make([]int, len(a)+1)
+		for i, v := range a {
+			arr[i+1] = v.(int)
+		}
+		rank := rankParksWills0(arr)
+		set := testData.set
+		if rank > len(set) || set[rank] {
+			testData.t.Errorf("duplicate permutation = %v", rank)
+		}
+		set[rank] = true
+	}
+
+	type args struct {
+		run  HeapCallback
+		a    []int
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		{
+			"random 3",
+			args{
+				run,
+				[]int{3, 1, 2},
+			},
+		},
+		{
+			"basic 6",
+			args{
+				run,
+				[]int{1, 2, 3, 4, 5, 6},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			n := len(tt.args.a)
+			array := make([]interface{}, n)
+			for i, v := range tt.args.a {
+				array[i] = v
+			}
+			var factorial = []int{1, 1, 2, 6, 24, 120, 720, 5040}
+			Heap(&TestData{t, make([]bool, factorial[n])}, tt.args.run, array, n)
+		})
+	}
 }
--- parkswills.go
+++ parkswills.go
@@ -7,14 +7,14 @@ package math
 // this maps a permutation of 1..n to x \in Z, where 0 <= x < n!
 // Inputs : a 1-indexed array, i.e. the 0th element is ignored.
 func rankParksWills0(arr []int) int {
-    r := 0
-    for k := len(arr) - 1; k > 1; k-- {
-        i := arr[k]
-        for i > k {
-            i = arr[i]
-        }
-        arr[k] = i // makes this O(n) and not O(n^2)
-        r = (k - i) + k*r
-    }
-    return r
+	r := 0
+	for k := len(arr) - 1; k > 1; k-- {
+		i := arr[k]
+		for i > k {
+			i = arr[i]
+		}
+		arr[k] = i // makes this O(n) and not O(n^2)
+		r = (k - i) + k*r
+	}
+	return r
 }
